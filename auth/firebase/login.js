008567286d60264bfb740351958500bc7979a3b3
mainlycricket <metusharjain@gmail.com>
2023-01-26 19:51:41 +0530

    Register & Login

---
+++ auth/firebase/login.js
@@ -0,0 +1,79 @@
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
+
+import { successMessage, failMessage } from "./index.js";
+
+const loginForm = document.getElementById("login-form");
+const googleSignUp = document.getElementById("googleSignUp");
+const logoutBtn = document.getElementById("logoutBtn");
+
+const auth = getAuth();
+
+if (loginForm) {
+  loginForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const formProps = new FormData(event.target);
+    const formData = Object.fromEntries(formProps);
+
+    const { email, password } = formData;
+
+    if (!email?.trim() || !password?.trim()) {
+      failMessage("Please provide email and password!");
+    }
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+      console.log(user.uid);
+      successMessage("Logged in successfully!");
+    } catch (error) {
+      console.log(error);
+      failMessage("Login failed!");
+    }
+  });
+}
+
+if (googleSignUp) {
+  googleSignUp.addEventListener("click", async function (event) {
+    event.preventDefault();
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const userId = result.user.uid;
+      console.log(userId);
+      successMessage("Google Auth Successful!");
+    } catch (error) {
+      failMessage(error.message);
+      console.log(error);
+    }
+  });
+}
+
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", async function (event) {
+    event.preventDefault();
+    await signOut(auth);
+  });
+}
+
+onAuthStateChanged(auth, (user) => {
+  const indexPage = "/";
+  const loginPage = "/auth/login";
+  const currentPage = location.pathname;
+
+  if (user) {
+    if (currentPage.startsWith(loginPage)) {
+      location.pathname = indexPage;
+    }
+  } else {
+    if (!currentPage.startsWith(loginPage)) {
+      location.pathname = loginPage + ".html";
+    }
+  }
+});
